48b5cacf028d9252f69d77c7b51b08b139a886bc
ChrisN-Tecsup <167912144+ChrisN-Tecsup@users.noreply.github.com>
2025-11-08 23:25:33 -0500

    Crear DbContext con tablas y relaciones

--- WebArt.Infrastructure/Persistence/WebArtDbContext.cs
+++ WebArt.Infrastructure/Persistence/WebArtDbContext.cs
@@ -1,6 +1,30 @@
-namespace WebArt.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using WebArt.Domain.Entities;
 
-public class WebArtDbContext
+namespace WebArt.Infrastructure.Persistence;
 {
-    
+public class WebArtDbContext : DbContext
+{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<AdminLog> AdminLogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+        }
+    }
 }
