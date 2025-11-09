220dcba564a840618d5c22a1ec29cea9ac30c31b
AlexisLopinta <alexis.lopinta@tecsup.edu.pe>
2025-11-09 00:04:41 -0500

    implementa WebArtDbContext y UnitOfWork

--- WebArt.Infrastructure/DependencyInjection.cs
+++ WebArt.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,34 @@
-namespace WebArt.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebArt.Application.Interfaces; // <-- Interfaz de Application
+using WebArt.Infrastructure.Persistence;
+using WebArt.Infrastructure.UnitOfWork; // <-- Tu implementación
 
-public class DependencyInjection
+namespace WebArt.Infrastructure
 {
-    
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            // 1. Configurar el DbContext (SQLite)
+            services.AddDbContext<WebArtDbContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(WebArtDbContext).Assembly.FullName)));
+
+            // 2. Registrar tu UnitOfWork
+            // Cuando un servicio (UserService) pida IUnitOfWork, 
+            // el sistema le entregará tu implementación (UnitOfWork).
+            services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
+            
+            // NOTA: No necesitamos registrar IRepository<> porque el UnitOfWork
+            // es el único que los crea y los entrega.
+            
+            // (Aquí también irían los servicios de Integrante 4: JWT, PasswordHasher, etc.)
+
+            return services;
+        }
+    }
 }
--- WebArt.Infrastructure/Persistence/WebArtDbContext.cs
+++ WebArt.Infrastructure/Persistence/WebArtDbContext.cs
@@ -1,6 +1,28 @@
-namespace WebArt.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using WebArt.Domain.Entities; 
 
-public class WebArtDbContext
+namespace WebArt.Infrastructure.Persistence
 {
-    
+    public class WebArtDbContext : DbContext
+    {
+        public WebArtDbContext(DbContextOptions<WebArtDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Artwork> Artworks { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+      
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
 }
